a4526279a611e52e3466d1be304674dcf56d2e09
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-05-16 11:21:29 -0600

    Created cosine_similarity file.

---
+++ src/similarities/cosine_similarity.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+class TFIDFSim(object) : 
+
+    def __init__(self) -> None:
+        pass
+
+    def fit(self) -> None:
+        pass
