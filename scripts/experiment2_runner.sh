0c0b0cf5b77e76865cd78308504b89fe2e9d47df
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2022-08-14 14:12:28 -0600

    Script runner for slurm experiment2

--- scripts/experiment2_runner.sh
+++ scripts/experiment2_runner.sh
@@ -2,20 +2,24 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=12:00:00
-#SBATCH --partition=shas
-#SBATCH --ntasks=24
+#SBATCH --partition=sgpu
 #SBATCH --job-name=experiment2-job
 #SBATCH --output=experiment2-job.%j.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dasr8731@colorado.edu
 
-# module purge
-# module load cuda
-# module load cudnn
+module purge
+module load cuda
+module load cudnn
 
-# nvidia-smi
+nvidia-smi
+
+export SENTENCE_TRANSFORMERS_HOME=/scratch/summit/dasr8731/models
+export TRANSFORMERS_CACHE=/scratch/summit/dasr8731/models
 
 cd /scratch/summit/dasr8731/needfinder
 source activate ./venv/ 
 
-python -m src.experiments.experiment2_autoeval
+python -m src.experiment2.runner
+
+##SBATCH --ntasks=24
