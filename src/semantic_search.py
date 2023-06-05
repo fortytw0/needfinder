5349122d9cc6405d5a9764a576d78de63825fa14
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2023-06-05 12:43:27 -0600

    store results as csv, prettify txt output

--- src/semantic_search.py
+++ src/semantic_search.py
@@ -1,6 +1,6 @@
 from sentence_transformers import SentenceTransformer, util
 import torch
-import string
+import pandas as pd
 import glob
 
 quote_path = 'data/osint_quotes.txt'
@@ -19,7 +19,7 @@ def pre_process_quotes(quotes) :
     processed_quotes = [] 
 
     for q in quotes : 
-        if q != '\n' : 
+        if q.strip() != '' : 
             processed_quotes.append(q)
 
     print(f'Len of quotes after processing is {len(processed_quotes)}')
@@ -40,6 +40,9 @@ model = SentenceTransformer('all-mpnet-base-v2')
 comment_embeddings = model.encode(comments, convert_to_tensor=True)
 
 f =  open('data/osint_results.txt' , 'w') 
+results = []
+result = {}
+
 
 for quote in quotes: 
 
@@ -47,13 +50,22 @@ for quote in quotes:
     cos_scores = util.cos_sim(quote_embedding, comment_embeddings)[0]
     top_results = torch.topk(cos_scores, k=30)
 
+    result['quote'] = quote
+
     f.write("\n\n======================\n\n")
     f.write("Quote: " + quote)
     f.write("\nTop 5 most similar sentences in corpus:")
 
-    for score, idx in zip(top_results[0], top_results[1]):
-        f.write(comments[idx] + "(Score: {:.4f})".format(score))
+    for i , score, idx in enumerate(zip(top_results[0], top_results[1])):
+        f.write('\n' + comments[idx] + "(Score: {:.4f})".format(score))
+        result[f'rank_{i+1}'] = comments[idx]
+        result[f'sim_score_{i+1}'] = score
+
+    results.append(result)
+    result = {}
 
 f.flush()
 f.close()
+
+pd.DataFrame(results).to_csv('data/osint_results.csv')
 
